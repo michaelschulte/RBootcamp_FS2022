4f4eb4c97cfac121be43497fe145dd2dccfd2996
Michael Schulte <michael@MacBook-Disaster-2.fritz.box>
2022-09-09 15:13:07 +0200

    render site updated

--- Render_whole_site.R
+++ Render_whole_site.R
@@ -1,4 +1,12 @@
 library(rmarkdown)
 library(tidyverse)
+files <- 
+list.files(path = '_sessions',
+           recursive = TRUE,
+           pattern = '\\.Rmd',
+           include.dirs = TRUE)
 
-source('render_files.R')
+
+for (i in 1:length(files)){
+  rmarkdown::render(paste0('_sessions/', files[i]))
+}
